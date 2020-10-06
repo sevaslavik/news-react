b64d75a247a4ade3ccf1ca1e89619051fc42a7c1
sevaslavik <seva5000Pro@gmail.com>
2020-10-06 11:40:54 +0300

    Deploy

---
+++ precache-manifest.92fa9262cda0a4765657092d1aa4a144.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5d9d6a6f3a889967010089d175d6e7",
+    "url": "/news-react/index.html"
+  },
+  {
+    "revision": "86bf3a6f35e3e24c6836",
+    "url": "/news-react/static/css/main.cfe7da71.chunk.css"
+  },
+  {
+    "revision": "d463e379a0e6a8ce68e1",
+    "url": "/news-react/static/js/2.3ec1cf4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/news-react/static/js/2.3ec1cf4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86bf3a6f35e3e24c6836",
+    "url": "/news-react/static/js/main.cc6120a0.chunk.js"
+  },
+  {
+    "revision": "6191f122c9f025cf3330",
+    "url": "/news-react/static/js/runtime-main.83680398.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/news-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
